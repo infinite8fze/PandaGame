5ba32477ec820f1af8519e6c5b7452febd9832ad
hossein goodarzi <hosseing27@gmail.com>
2025-03-08 13:19:14 +0330

    feat: add extra object of bathroom

--- src/components/rooms/Bathroom.tsx
+++ src/components/rooms/Bathroom.tsx
@@ -10,13 +10,42 @@ interface BathroomProps {
 }
 
 export function Bathroom({ children, character }: BathroomProps) {
-  const [isMinigameModalOpen, setIsMinigameModalOpen] = useState(false);
-  const [currentGameUrl, setCurrentGameUrl] = useState<string | null>(null);
+  const outsideObjects = (
+    <>
+      {/* Stove on the left */}
+      <div className="absolute extra-sm:left-[-30%] md:left-0 lg:left-[27%] top-[77%] translate-y-[-100%] translate-x-[-50%]">
+        <img
+          src="/images/rooms/bathroom/bath.png"
+          alt="Bath"
+          className="w-auto extra-sm:h-72 md:h-80"
+        />
+        <img
+          src="/images/rooms/bathroom/Stool.png"
+          alt="Stool"
+          className="w-50 h-auto absolute translate-y-[-70%] translate-x-[-50%] extra-sm:left-[50%] md:left-[45%] lg:left-[47%] top-[100%]"
+        />
+        <img
+          src="/images/rooms/bathroom/Soap2.png"
+          alt="Soap"
+          className="extra-sm:w-20 md:w-24 lg:w-24 h-auto absolute translate-y-[-70%] translate-x-[-50%] extra-sm:left-[66%] md:left-[56%] lg:left-[60%] top-[65%]"
+        />
+        <img
+          src="/images/rooms/bathroom/Soap.png"
+          alt="Soap"
+          className="extra-sm:w-20 md:w-24 lg:w-20 h-auto absolute translate-y-[-70%] translate-x-[-50%] extra-sm:left-[66%] md:left-[40%] lg:left-[45%] top-[65%]"
+        />
+      </div>
 
-  const handleGameSelect = (url: string) => {
-    setCurrentGameUrl(url);
-    setIsMinigameModalOpen(false);
-  };
+      {/* Toilet on the right */}
+      <div className="absolute extra-sm:right-[20%] md:right-[20%] lg:right-[37%] top-[78%] translate-y-[-100%] translate-x-1/2">
+        <img
+          src="/images/rooms/bathroom/toilet.png"
+          alt="Toilet"
+          className="w-56 h-auto"
+        />
+      </div>
+    </>
+  );
 
   return (
     <Layout
@@ -24,18 +53,8 @@ export function Bathroom({ children, character }: BathroomProps) {
         <RoomBackground room="bathroom" translateY="translate-y-[-1.5%]" />
       }
       character={character}
+      outsideObjects={outsideObjects}
     >
-      <MinigameModal
-        isOpen={isMinigameModalOpen}
-        onClose={() => setIsMinigameModalOpen(false)}
-        onSelectGame={handleGameSelect}
-      />
-      {currentGameUrl && (
-        <GameFrame
-          url={currentGameUrl}
-          onClose={() => setCurrentGameUrl(null)}
-        />
-      )}
       {children}
     </Layout>
   );
