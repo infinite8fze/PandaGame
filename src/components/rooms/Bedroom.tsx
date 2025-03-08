5aaf6222d1e5ae88b6200476e095acb2bc8d90c9
hossein goodarzi <hosseing27@gmail.com>
2025-03-08 13:52:29 +0330

    feat: add extra object of bedroom

--- src/components/rooms/Bedroom.tsx
+++ src/components/rooms/Bedroom.tsx
@@ -10,27 +10,24 @@ interface BedroomProps {
 }
 
 export function Bedroom({ children, character }: BedroomProps) {
-  const [isMinigameModalOpen, setIsMinigameModalOpen] = useState(false);
-  const [currentGameUrl, setCurrentGameUrl] = useState<string | null>(null);
-
-  const handleGameSelect = (url: string) => {
-    setCurrentGameUrl(url);
-    setIsMinigameModalOpen(false);
-  };
-
   const outsideObjects = (
     <>
       {/* Wardrobe on the right */}
-      <div className="absolute left-1/3 top-2/3 translate-y-[-90%] translate-x-[-50%]">
+      <div className="absolute extra-sm:left-[-20%] md:left-[10%] lg:left-[32%]  extra-sm:top-[80%] md:top-[79%] translate-y-[-100%] translate-x-[-50%]">
         <img
           src="/images/rooms/bedroom/wardrobe.png"
           alt="Wardrobe"
-          className="w-96 h-auto"
+          className=" h-auto"
+        />
+        <img
+          src="/images/rooms/bedroom/left-table.png"
+          alt="Wardrobe"
+          className=" h-auto absolute translate-y-[-105%] left-[-70%] "
         />
       </div>
 
       {/* Bed on the left */}
-      <div className="absolute right-1/3 top-2/3 translate-y-[-90%] translate-x-[70%]">
+      <div className="absolute extra-sm:right-[-140%] md:right-[-55%] lg:right-[5%] top-[80%] translate-y-[-100%] translate-x-[-50%]">
         <img
           src="/images/rooms/bedroom/bed.png"
           alt="Bed"
@@ -39,7 +36,7 @@ export function Bedroom({ children, character }: BedroomProps) {
       </div>
 
       {/* Light at the top */}
-      <div className="absolute right-0 top-1/2 -translate-x-1/2 translate-y-[-50%]">
+      <div className="absolute extra-sm:right-[-80%] lg:right-[5%] top-[75%] -translate-x-1/2 translate-y-[-100%]">
         <img
           src="/images/rooms/bedroom/light.png"
           alt="Light"
@@ -52,22 +49,11 @@ export function Bedroom({ children, character }: BedroomProps) {
   return (
     <Layout
       background={
-        <RoomBackground room="bedroom" translateY="translate-y-[-33%]" />
+        <RoomBackground room="bedroom" translateY="translate-y-[-61%]" />
       }
       character={character}
       outsideObjects={outsideObjects}
     >
-      <MinigameModal
-        isOpen={isMinigameModalOpen}
-        onClose={() => setIsMinigameModalOpen(false)}
-        onSelectGame={handleGameSelect}
-      />
-      {currentGameUrl && (
-        <GameFrame
-          url={currentGameUrl}
-          onClose={() => setCurrentGameUrl(null)}
-        />
-      )}
       {children}
     </Layout>
   );
